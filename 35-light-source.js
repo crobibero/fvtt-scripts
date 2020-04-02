10e8d835daafb79bf3f237b0cd2b8e142a6d1023
crobibero <cody@robibe.ro>
2020-04-02 10:51:57 -0600

    add 3.5 darksight

--- 35-light-source.js
+++ 35-light-source.js
@@ -4,6 +4,15 @@ new Dialog({
   content: `
     <form>
       <div class="form-group">
+        <label>Vision Type:</label>
+        <select id="vision-type" name="vision-type">
+          <option value="nochange">No Change</option>
+          <option value="dim0">Self</option>
+          <option value="dim60">Darkvision</option>
+          <option value="dim120">Darkvision (Drow)</option>
+        </select>
+      </div>
+      <div class="form-group">
         <label>Light Source:</label>
         <select id="light-source" name="light-source">
           <option value="nochange">No Change</option>
@@ -33,11 +42,33 @@ new Dialog({
   close: html => {
     if (applyChanges) {
       for ( let token of canvas.tokens.controlled ) {
+        let visionType = html.find('[name="vision-type"]')[0].value || "none";
         let lightSource = html.find('[name="light-source"]')[0].value || "none";
+        let dimSight = 0;
+        let brightSight = 0;
         let dimLight = 0;
         let brightLight = 0;
         let lightAngle = 360;
         let lockRotation = token.data.lockRotation;
+        // Get Vision Type Values
+        switch (visionType) {
+          case "dim0":
+            dimSight = 0;
+            brightSight = 0;
+            break;
+          case "dim60":
+            dimSight = 60;
+            brightSight = 0;
+            break;
+          case "dim120":
+            dimSight = 120;
+            brightSight = 0;
+            break;
+          case "nochange":
+          default:
+            dimSight = token.data.dimSight;
+            brightSight = token.data.brightSight;
+        }
         // Get Light Source Values
         switch (lightSource) {
           case "none":
@@ -85,6 +116,8 @@ new Dialog({
         console.debug(token);
         token.update({
           vision: true,
+          dimSight: dimSight,
+          brightSight: brightSight,
           dimLight: dimLight,
           brightLight:  brightLight,
           lightAngle: lightAngle,
